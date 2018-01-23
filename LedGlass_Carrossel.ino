e818807da276b82fad72612739970a38f3563877
Evandro Peixoto da Silva <evandropeixoto@msn.com>
2018-01-23 18:37:11 -0200

    Add files via upload

---
+++ LedGlass_Carrossel.ino
@@ -0,0 +1,43 @@
+// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
+// released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
+#include <Adafruit_NeoPixel.h>
+
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN            4
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      24
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+// example for more information on possible values.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_RGB + NEO_KHZ800);
+
+int delayval = 1; // delay for half a second
+int R =100;
+int G =10;
+
+void setup() {
+  pixels.begin(); // This initializes the NeoPixel library.
+}
+
+void loop() {
+  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
+  for(int i=0;i<NUMPIXELS;i++){
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    for(int r=0;r <=255;r++){
+  
+    // Na linha abaixo é onde você enviar a cor para o oculos 
+    
+    pixels.setPixelColor(i, pixels.Color(r, 100, 0)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval); // Delay for a period of time (in milliseconds).
+    }
+  }
+    for(int i=NUMPIXELS;i>=0;i--){
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0,0,0)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval); // Delay for a period of time (in milliseconds).
+  }
+}
